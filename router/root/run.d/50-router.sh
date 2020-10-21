2e0c50db292a99f119479135099faf1fa50c16d9
Ildar Bikmamatov <ildar@bayrell.org>
2020-10-21 11:05:38 +0000

    flush timestamp nginx changes when container start

--- router/root/run.d/50-router.sh
+++ router/root/run.d/50-router.sh
@@ -1,3 +1,5 @@
+echo "0" > /data/nginx.changes.last
+
 if [ ! -z $CLOUD_PANEL ] && [ ! -z $CLOUD_DOMAIN ]; then
 	sed -i "s|%CLOUD_PANEL%|${CLOUD_PANEL}|g" /etc/nginx/conf.d/99-upstreams.conf
 	sed -i "s|%CLOUD_PANEL%|${CLOUD_PANEL}|g" /etc/nginx/sites-available/50-system-panel.conf
