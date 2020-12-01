84ccefc1a85f5c16a159dc391fe1131cd20386d6
Ildar Bikmamatov <ildar@bayrell.org>
2020-12-01 17:12:13 +0000

    rename to load_balancer_http

--- build.sh
+++ build.sh
@@ -5,31 +5,34 @@ SCRIPT_PATH=`dirname $SCRIPT`
 BASE_PATH=`dirname $SCRIPT_PATH`
 
 RETVAL=0
-VERSION=0.1.0
+VERSION=0.2.0
 TAG=`date '+%Y%m%d_%H%M%S'`
 
 case "$1" in
 	
 	test)
-		docker build ./ -t bayrell/cloud_router_http:$VERSION-$TAG --file Dockerfile
+		docker build ./ -t bayrell/load_balancer_http:$VERSION-$TAG --file Dockerfile
 		cd ..
 	;;
 	
 	amd64)
-		docker build ./ -t bayrell/cloud_router_http:$VERSION-amd64 --file Dockerfile --build-arg ARCH=-amd64
-		docker push bayrell/cloud_router_http:$VERSION-amd64
+		docker build ./ -t bayrell/load_balancer_http:$VERSION-amd64 --file Dockerfile --build-arg ARCH=-amd64
 	;;
 	
 	arm32v7)
-		docker build ./ -t bayrell/cloud_router_http:$VERSION-arm32v7 --file Dockerfile --build-arg ARCH=-arm32v7
-		docker push bayrell/cloud_router_http:$VERSION-arm32v7
+		docker build ./ -t bayrell/load_balancer_http:$VERSION-arm32v7 --file Dockerfile --build-arg ARCH=-arm32v7
 	;;
 	
 	manifest)
-		docker manifest create --amend bayrell/cloud_router_http:$VERSION \
-			bayrell/cloud_router_http:$VERSION-amd64 \
-			bayrell/cloud_router_http:$VERSION-arm32v7
-		docker manifest push --purge bayrell/cloud_router_http:$VERSION
+		rm -rf ~/.docker/manifests/docker.io_load_balancer_http-*
+		
+		docker push bayrell/load_balancer_http:$VERSION-amd64
+		docker push bayrell/load_balancer_http:$VERSION-arm32v7
+		
+		docker manifest create --amend bayrell/load_balancer_http:$VERSION \
+			bayrell/load_balancer_http:$VERSION-amd64 \
+			bayrell/load_balancer_http:$VERSION-arm32v7
+		docker manifest push --purge bayrell/load_balancer_http:$VERSION
 	;;
 	
 	all)
--- network.create.sh
+++ network.create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker network create --subnet 172.21.0.1/16 --driver=overlay --attachable cloud_router -o "com.docker.network.bridge.name"="cloud_router"
+docker network create --subnet 172.21.0.1/16 --driver=overlay --attachable load_balancer -o "com.docker.network.bridge.name"="load_balancer"
 
 docker network create --subnet 172.22.0.1/16 --driver=overlay --attachable cloud_admin -o "com.docker.network.bridge.name"="cloud_admin"
 
