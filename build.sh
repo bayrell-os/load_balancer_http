bd798187c3e88af7b7eb95e2e5e08ddeddcfdd84
Ildar Bikmamatov <ildar@bayrell.org>
2021-02-10 18:41:50 +0000

    add arm64

--- build.sh
+++ build.sh
@@ -19,6 +19,10 @@ case "$1" in
 		docker build ./ -t bayrell/load_balancer_http:$VERSION-amd64 --file Dockerfile --build-arg ARCH=-amd64
 	;;
 	
+	arm64v8)
+		docker build ./ -t bayrell/load_balancer_http:$VERSION-arm64v8 --file Dockerfile --build-arg ARCH=-arm64v8
+	;;
+	
 	arm32v7)
 		docker build ./ -t bayrell/load_balancer_http:$VERSION-arm32v7 --file Dockerfile --build-arg ARCH=-arm32v7
 	;;
@@ -27,22 +31,25 @@ case "$1" in
 		rm -rf ~/.docker/manifests/docker.io_load_balancer_http-*
 		
 		docker push bayrell/load_balancer_http:$VERSION-amd64
+		docker push bayrell/load_balancer_http:$VERSION-arm64v8
 		docker push bayrell/load_balancer_http:$VERSION-arm32v7
 		
 		docker manifest create --amend bayrell/load_balancer_http:$VERSION \
 			bayrell/load_balancer_http:$VERSION-amd64 \
+			bayrell/load_balancer_http:$VERSION-arm64v8 \
 			bayrell/load_balancer_http:$VERSION-arm32v7
 		docker manifest push --purge bayrell/load_balancer_http:$VERSION
 	;;
 	
 	all)
 		$0 amd64
+		$0 arm64v8
 		$0 arm32v7
 		$0 manifest
 	;;
 	
 	*)
-		echo "Usage: $0 {amd64|arm32v7|manifest|all|test}"
+		echo "Usage: $0 {amd64|arm64v8|arm32v7|manifest|all|test}"
 		RETVAL=1
 
 esac
