79cd6597ef108537372baf87b6bdbb0bbf671979
Ildar Bikmamatov <ildar@bayrell.org>
2021-10-23 16:02:23 +0000

    rename to cloud_router_http

--- build.sh
+++ build.sh
@@ -11,34 +11,34 @@ TAG=`date '+%Y%m%d_%H%M%S'`
 case "$1" in
 	
 	test)
-		docker build ./ -t bayrell/load_balancer_http:$VERSION-$TAG --file Dockerfile
+		docker build ./ -t bayrell/cloud_router_http:$VERSION-$TAG --file Dockerfile
 		cd ..
 	;;
 	
 	amd64)
-		docker build ./ -t bayrell/load_balancer_http:$VERSION-amd64 --file Dockerfile --build-arg ARCH=-amd64
+		docker build ./ -t bayrell/cloud_router_http:$VERSION-amd64 --file Dockerfile --build-arg ARCH=-amd64
 	;;
 	
 	arm64v8)
-		docker build ./ -t bayrell/load_balancer_http:$VERSION-arm64v8 --file Dockerfile --build-arg ARCH=-arm64v8
+		docker build ./ -t bayrell/cloud_router_http:$VERSION-arm64v8 --file Dockerfile --build-arg ARCH=-arm64v8
 	;;
 	
 	arm32v7)
-		docker build ./ -t bayrell/load_balancer_http:$VERSION-arm32v7 --file Dockerfile --build-arg ARCH=-arm32v7
+		docker build ./ -t bayrell/cloud_router_http:$VERSION-arm32v7 --file Dockerfile --build-arg ARCH=-arm32v7
 	;;
 	
 	manifest)
-		rm -rf ~/.docker/manifests/docker.io_load_balancer_http-*
+		rm -rf ~/.docker/manifests/docker.io_cloud_router_http-*
 		
-		docker push bayrell/load_balancer_http:$VERSION-amd64
-		docker push bayrell/load_balancer_http:$VERSION-arm64v8
-		docker push bayrell/load_balancer_http:$VERSION-arm32v7
+		docker push bayrell/cloud_router_http:$VERSION-amd64
+		docker push bayrell/cloud_router_http:$VERSION-arm64v8
+		docker push bayrell/cloud_router_http:$VERSION-arm32v7
 		
-		docker manifest create --amend bayrell/load_balancer_http:$VERSION \
-			bayrell/load_balancer_http:$VERSION-amd64 \
-			bayrell/load_balancer_http:$VERSION-arm64v8 \
-			bayrell/load_balancer_http:$VERSION-arm32v7
-		docker manifest push --purge bayrell/load_balancer_http:$VERSION
+		docker manifest create --amend bayrell/cloud_router_http:$VERSION \
+			bayrell/cloud_router_http:$VERSION-amd64 \
+			bayrell/cloud_router_http:$VERSION-arm64v8 \
+			bayrell/cloud_router_http:$VERSION-arm32v7
+		docker manifest push --purge bayrell/cloud_router_http:$VERSION
 	;;
 	
 	all)
